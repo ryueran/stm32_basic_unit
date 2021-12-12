e9243b7a673b27f01a8a614bbfa419840b9db164
ryueran <yueranl03@gmail.com>
2021-12-12 19:53:23 +0100

    Update of test case

--- stm32_vs_2/APP/tests/test_example.c
+++ stm32_vs_2/APP/tests/test_example.c
@@ -1,3 +1,4 @@
+#include "stdlib.h"
 #include "unity.h"
 #include "fff.h"
 #include "stm32f0xx_hal_flash.h"
@@ -12,10 +13,14 @@ FAKE_VALUE_FUNC0(uint32_t, HAL_FLASH_GetError);
 FAKE_VALUE_FUNC0(HAL_StatusTypeDef, HAL_FLASH_Lock);
 FAKE_VALUE_FUNC3(HAL_StatusTypeDef, HAL_FLASH_Program, uint32_t, uint32_t, uint64_t);
 
+uint32_t *PAGEError = 0;
+static FLASH_EraseInitTypeDef EraseInitStruct;
+
 void setUp(void)
 {
   /* This is run before EACH TEST */
   FFF_RESET_HISTORY();
+
 }
 
 void tearDown(void)
@@ -43,9 +48,16 @@ void test_case4(void)
 void test_case2(void)
 {
     uint32_t data_write[] = {};
+    EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+    EraseInitStruct.PageAddress = 0x08000000;
+    EraseInitStruct.NbPages = 1;
     Flash_Write_Data(0x08000000, data_write, 4);
     TEST_ASSERT_EQUAL(HAL_FLASH_Lock_fake.call_count, 1);
     TEST_ASSERT_EQUAL(HAL_FLASH_Unlock_fake.call_count, 1);
+    TEST_ASSERT_EQUAL(HAL_FLASHEx_Erase_fake.call_count, 1);
+    TEST_ASSERT_EQUAL(HAL_FLASHEx_Erase_fake.arg0_val->TypeErase, FLASH_TYPEERASE_PAGES);
+    TEST_ASSERT_EQUAL(HAL_FLASHEx_Erase_fake.arg0_val->PageAddress, 0x08000000);    
+    TEST_ASSERT_EQUAL(HAL_FLASHEx_Erase_fake.arg0_val->NbPages, 1);
 }
 
 int main(int argc, const char * argv[])
@@ -54,4 +66,5 @@ int main(int argc, const char * argv[])
     RUN_TEST(test_case2);
     RUN_TEST(test_case3);
     RUN_TEST(test_case4);
+    return 1;
 }
