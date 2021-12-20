811f646995b7f2bcac553d7e97115b3fb1bba6f9
ryueran <yueranl03@gmail.com>
2021-12-20 22:31:45 +0100

    Change parity to event

--- stm32_vs_2/APP/HAL/uart_munipulation.c
+++ stm32_vs_2/APP/HAL/uart_munipulation.c
@@ -11,9 +11,9 @@ void UART_Init()
 {
     huart2.Instance = USART2;
     huart2.Init.BaudRate = 9600;
-    huart2.Init.WordLength = UART_WORDLENGTH_8B;
+    huart2.Init.WordLength = UART_WORDLENGTH_9B;
     huart2.Init.StopBits = UART_STOPBITS_1;
-    huart2.Init.Parity = UART_PARITY_NONE;
+    huart2.Init.Parity = UART_PARITY_EVEN;
     huart2.Init.Mode = UART_MODE_TX_RX;
     huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart2.Init.OverSampling = UART_OVERSAMPLING_16;
